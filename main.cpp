01961d0f1682c32c7e40f03c55f570c4b6c1927e
RGioele <116067942+RGioele@users.noreply.github.com>
2022-12-23 11:09:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,78 +1,39 @@
+
 #include <iostream>
-#include "math.h"
+#include <cmath>
 using namespace std;
 
-bool funz (float &a, float &b){
- float fa, fb;
- fa=pow(a,2)*cos(a)+1;
- fb=pow(b, 2)*cos(b)+1;
-    if ((fa*fb)>=0){
-        return true;}
-    else {
-        return false;}
+float funz( float a){
+    float y=0;
+    y= ((a*a)* cos(a))+1;
+    return y;
 }
-
-bool funz_1 (float &a, float &b, float &fx){
-    float fa, fb;
-
-    fa=pow(a,2)*cos(a)+1;
-    fb=pow(b, 2)*cos(b)+1;
-    fx=fa*fb;
-
-    if ((fa*fb)==0){
-        return true;}
-    else {
-        return false;}
-}
-
-bool funz_2 (float &a, float &b){
-    float fa, fb;
-    fa=pow(a,2)*cos(a)+1;
-    fb=pow(b, 2)*cos(b)+1;
-    if ((fa*fb)<=0){
-        return true;}
-    else {
-        return false;}
-}
-
-bool err (float &a, float &b, float &x){
-    float err;
-    err=abs((b-a)/2);
-    if ((err)>=1 || (err)>=-6 ){
-        return true;}
-    else {
-        return false;}
-}
-float x;
-
 
 int main() {
-    float a, b, fx;
-    a=0;
-    b=0;
-    fx=0;
-    do {
-    cout<<"inserire estremi"<<endl;
-    cin>>a>>b;
-    }
-    while (funz(a, b));
-    do {
-    a=(a+b)/2;
-
-    x=(a+b)/2;
-    if (funz_1(a, b, fx)) {
-        cout << fx << " " << x << endl;
-    }
-    else if (funz_2(a, b)){
-        a=x;
-    }
-    else if (funz_2(a, b)==false){
-        b=x;
-
+    float a=0, b=0, m=0,err=1;
+    while((funz(a)* funz(b))>=0){
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
     }
 
+    while (funz(m)!=0){
+        m=((a+b)/2);
+        if ((funz(m)*funz(b))<0){
+            a=m;
+        }else{
+            b=m;
+        }
+        err=((b-a)/2);
+        if (err<(1e-6)){
+            break;
+        }
     }
-    while (err(a, b, x));
-    cout<<x<<fx;
-   return 0;
+
+    m=m*10000;
+    m=(int)m;
+    m=m/10000;
+    cout<< m;
+
+
+    return 0;
 }
